357d72f11aef59b0bd5b9c10cdc5a0a01b4d64ee
tranphuquy19 <christian@enclave.vn>
2021-08-08 02:03:25 +0700

    update

--- error/error.go
+++ error/error.go
@@ -4,12 +4,12 @@ import (
 	"net/http"
 )
 
-var(
+var (
 	ErrInvalidParam  = &RequestError{ErrorString: "Bad Request", ErrorCode: http.StatusBadRequest}
 	ErrInternalError = &RequestError{ErrorString: "Internal Error", ErrorCode: http.StatusInternalServerError}
-	ErrNotFound      = &RequestError{ErrorString: "Request Not Found", ErrorCode: http.StatusNotFound}
-	ErrUnauthorized = &RequestError{ErrorString: "Unauthorized", ErrorCode: http.StatusUnauthorized}
-	ErrForbidden = &RequestError{ErrorString: "Forbidden", ErrorCode: http.StatusForbidden}
+	ErrNotFound      = &RequestError{ErrorString: "Not Found", ErrorCode: http.StatusNotFound}
+	ErrUnauthorized  = &RequestError{ErrorString: "Unauthorized", ErrorCode: http.StatusUnauthorized}
+	ErrForbidden     = &RequestError{ErrorString: "Forbidden", ErrorCode: http.StatusForbidden}
 )
 
 type RequestError struct {
